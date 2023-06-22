ca1045e88e984436be66684695c0373f4369bc0c
baraa1999 <baraaalian7@gmail.com>
2023-06-22 14:11:01 +0300

    finch notes bottom sheet

--- lib/views/widget/add_note_bottom_sheet.dart
+++ lib/views/widget/add_note_bottom_sheet.dart
@@ -1,32 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:notesapp/views/widget/custom_text_field.dart';
 
+import 'custom_button.dart';
+
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
+    return  const Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 32,
-          ),
-          CustomTextField(
-            hint: 'title',
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 32,
+            ),
+            CustomTextField(
+              hint: 'title',
 
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          CustomTextField(
-            hint: 'content',
-            maxLines: 5,
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextField(
+              hint: 'content',
+              maxLines: 5,
 
-          ),
-        ],
+            ),
+            SizedBox(height: 32,),
+            CustomButton(),
+            SizedBox(height: 16,),
+          ],
+        ),
       ),
     );
   }
 }
+
+
